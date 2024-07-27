208b4d556126475e973da6a6beedb907ba68f181
6millions <kittichai.s@ic-connection.com>
2024-07-28 00:38:37 +0700

    finish practice for today

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# TS-Challenge-Template
-Template of TS Challenge
+# TS-D14-PlayWithFunctionType
+Practice Array In TypeScript
+
+## Overview
+Play with function type
+
+## Installation
+1. Clone the repository
+2. Install dependencies: npm install
+
+## Problem Explanation
+1. Define a function type
+2. map and use it
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+# Special Note
+Include `launch.json` to version control, for the purpose of this project and as a learning resource for debugging TypeScript projects with Visual Studio Code.
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,32 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+
+    const msg = testMessage();
+    res.send(msg);
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+function testMessage(): string {
+
+    console.log('[testMessage]');
+    let addNumber: (a: number, b: number) => number;
+
+    addNumber = add;
+
+    let resultTest = addNumber(7, 10);
+
+    return "result is " + resultTest;
+}
+
+function add(input1: number, input2: number): number {
+
+    console.log('[add] input1 = ' + input1 + ', input2 = ' + input2);
+    const result = input1 + input2;
+
+    return result;
+
+}
